7b0215fc8881e2f1d68894ab155ab39d8a0ab6b1
IndumathiMR <indumathimr@hmgtechnology.com>
2023-11-09 10:52:19 +0530

    updated aboutData

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,8 @@
 export const aboutData = {
-    title: "Who I am",
-    description1: "My name's Indumathi. I'm frontEnd developer based in Coimbatore, TamilNadu",
-    description2: "During the day I work as a junior React developer at a local agency and in the evening I utilize the time to built my own products. I spent my leisure hours in reading novels. I love to learn and explore new arenas.",
-    image: "https://avatars.githubusercontent.com/u/75137227?v=4"
-}
+  title: "Who I am",
+  description1:
+    "My name's Indumathi. I'm frontEnd developer based in Coimbatore, TamilNadu",
+  description2:
+    "During the day I work as a React developer at a local agency and in the evening I utilize the time to built my own products. I spent my leisure hours in hearing music and watching movies. I love to learn and explore new arenas.",
+  image: "https://avatars.githubusercontent.com/u/75137227?v=4",
+};
